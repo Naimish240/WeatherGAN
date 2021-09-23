c079cb5cfb562e6a2810145b56a7f7399ba4c7d6
NaimishBalaji <naimish240@gmail.com>
2021-09-23 12:04:43 +0530

    commented out a few years to reduce download load

--- src/download.py
+++ src/download.py
@@ -7,17 +7,17 @@ import gdown
 folders = {
            1999: '1ObZwt6cb97XogQoKu5vQvcnOzeQX4h_5',
            2001: '1ishYRAffV0pdos8wufxQwC4y00b89sd4',
-           2002: '1_yoN_uqIcPv976jTJ3Aqyk1nuOPOaAvP',
+           # 2002: '1_yoN_uqIcPv976jTJ3Aqyk1nuOPOaAvP',
            2004: '1bnRoWv3jtXTJrd1waVsqiWd-1k24XtyB',
            2005: '1g9Ap2i-yVqia1SQ0R4pAmdB_jvRBqogj',
            2007: '1eScmenYHUFvPxL_fe1UmT_QiK8M_VZ6c',
            2008: '1eScmenYHUFvPxL_fe1UmT_QiK8M_VZ6c',
-           2009: '1ov0hQ3uDlg63K-zIbisE58QYyYgMtzOJ',
-           2010: '1b6qydpGDjSuLcA5E_E2gQi2zQYmrzeNn',
-           2011: '1R1I03oyEhk8WaAWhnYNsfsR9gDmfVugL',
-           2013: '1se8x4MWy-dbK5UrPvNoRmRELAK8n0C6r',
-           2014: '1HyY0PAzzlTG2ciy_zKJ1MOgsNIeNvLWi',
-           2015: '1Ou2jROi2gwXkknQChtjNWn35dVktxIBC'
+           # 2009: '1ov0hQ3uDlg63K-zIbisE58QYyYgMtzOJ',
+           # 2010: '1b6qydpGDjSuLcA5E_E2gQi2zQYmrzeNn',
+           # 2011: '1R1I03oyEhk8WaAWhnYNsfsR9gDmfVugL',
+           # 2013: '1se8x4MWy-dbK5UrPvNoRmRELAK8n0C6r',
+           # 2014: '1HyY0PAzzlTG2ciy_zKJ1MOgsNIeNvLWi',
+           # 2015: '1Ou2jROi2gwXkknQChtjNWn35dVktxIBC'
 }
 
 
